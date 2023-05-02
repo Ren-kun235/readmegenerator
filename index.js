4aae82a31dc80f156815a9475e8714a13474be87
Ren-Kun235 <changpobzeb651@gmail.com>
2023-05-02 16:02:18 -0500

    structered index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt()
+inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "name",
+        },
+        {
+            type: "",
+        },
+        {
+            type: "",
+        },
+    ])
+
+.then((answers) => {
+    fs.writeFile("README.md", 
+    ``
+    , err =>err ? console.log(err): console.log("README.md was created"))
+})
