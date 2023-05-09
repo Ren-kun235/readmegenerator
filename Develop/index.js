8d33ae506543270246ea775dcb2160cfb8cd9b07
Ren-Kun235 <changpobzeb651@gmail.com>
2023-05-09 18:33:53 -0500

    added questions, initial inquirer started

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,53 @@
-// TODO: Include packages needed for this application (done)
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: questions[0],
+        name: "Description"
+    },
+    {
+        type: "checkbox",
+        message: questions[1],
+        name: "Table of Contents"
+    },
+    {
+        type: "input",
+        message: questions[2],
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: questions[3],
+        name: "Usage"
+    },
+    {
+        type: "checkbox",
+        message: questions[4],
+        name: "License"
+    },
+    {
+        type: "input",
+        message: questions[5],
+        name: "Credit"
+    },
+])
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "Write a description of your web application",
+
+    "If you would like to include a Table of Contents, please select the following sections you would like to include in your README file.",
+
+    "What is the process of installation?",
+
+    "How would I, as the user, be able to access and use this application?",
+
+    "Licensed by..?",
+
+    "Credits given to...?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +57,9 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+.then((answers) => {
+    fs.writeFile("README.md", 
+    `
+    `)
+})
